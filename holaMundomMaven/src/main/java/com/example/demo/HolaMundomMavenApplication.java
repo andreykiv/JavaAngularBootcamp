e4291d4aa16c5161f2efb067ad8334b8906aa00b
Myroslav <andreykiv09@gmail.com>
2021-03-08 21:49:14 +0100

    init commit

---
+++ holaMundomMaven/src/main/java/com/example/demo/HolaMundomMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundomMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundomMavenApplication.class, args);
+	}
+
+}
