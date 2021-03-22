d657a8bc71f74ebc688e2ee2832fc592cd3b6fc8
Myroslav <andreykiv09@gmail.com>
2021-03-22 18:41:38 +0100

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './Persona/add/add.component';
+import { ListarComponent } from './Persona/listar/listar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'add', component:AddComponent},
+  {path: 'listar', component:ListarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarComponent } from './Persona/listar/listar.component';
+import { AddComponent } from './Persona/add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
